b16eaab5958c2c787ed06506f9f982f1382ccf13
Finn <finnmassari16@gmail.com>
2024-04-20 12:06:32 +0200

    fb connected

--- src/components/fbDir/fbConfig.js
+++ src/components/fbDir/fbConfig.js
@@ -1,1 +1,11 @@
 // src\components\fbDir\fbConfig.js
+
+export default {
+    apiKey: "AIzaSyA-87AscFDpaSHMF6yWfbajtPrpaDDFX1s",
+    authDomain: "coplaylist-e9ecd.firebaseapp.com",
+    projectId: "coplaylist-e9ecd",
+    storageBucket: "coplaylist-e9ecd.appspot.com",
+    messagingSenderId: "217434249078",
+    appId: "1:217434249078:web:25900abae2dd254f8f8c57",
+    measurementId: "G-7XZLQEKVMJ"
+}
--- src/components/fbDir/fbInit.js
+++ src/components/fbDir/fbInit.js
@@ -1,1 +1,20 @@
 // src\components\fbDir\fbInit.js
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore} from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+import fbConfig from './fbConfig';
+
+const firebaseApp = initializeApp(fbConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+const storage = getStorage(firebaseApp);
+
+// Export section 
+//---------------------------------------------------------------------------------------------------------------------------
+
+export {db, auth, storage}
+
+
+//---------------------------------------------------------------------------------------------------------------------------
