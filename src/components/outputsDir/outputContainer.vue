b023d6f674d2bfbf019b27dd1990d5c0f35d128a
WuTangLan1 <finnmassari16@gmail.com>
2024-04-25 09:45:02 +0200

    Adding output Section

---
+++ src/components/outputsDir/outputContainer.vue
@@ -0,0 +1,15 @@
+<!-- src\components\outputsDir\playlist\outputSection.vue -->
+
+
+<script>
+
+</script>
+
+<template>
+    <p>hello</p>
+
+</template>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import homeView from '../views/homeView.vue';
 import ToneContainer from '../components/inputsDir/toneContainer.vue';
 import VibeContainer from '../components/inputsDir/vibeContainer.vue';
 import SongsContainer from '../components/inputsDir/songsContainer.vue';
+import OutputContainer from '../components/outputsDir/outputContainer.vue'
 
 const routes = [
   {
@@ -25,6 +26,11 @@ const routes = [
     path: '/songs',
     name: 'Songs',
     component: SongsContainer
+  },
+  {
+    path: '/output',
+    name: 'Output',
+    component: OutputContainer
   }
 ];
 
