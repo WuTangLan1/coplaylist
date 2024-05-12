e0280e4119e28c94767ad43d110afbdedc50b0fd
Finn <finnmassari16@gmail.com>
2024-05-12 20:00:09 +0200

    Adding reg containers

---
+++ src/components/authDir/register/infoContainer.vue
@@ -0,0 +1,14 @@
+<!-- src\components\authDir\register\infoContainer.vue -->
+
+<script>
+
+</script>
+
+<template>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/authDir/register/preferencesContainer.vue
@@ -0,0 +1,14 @@
+<!-- src\components\authDir\register\preferencesContainer.vue -->
+
+<script>
+
+</script>
+
+<template>
+
+
+</template>
+
+<style scoped>
+
+</style>
