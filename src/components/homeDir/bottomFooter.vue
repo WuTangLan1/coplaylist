f27d1589341bde89dde6cb7fddc68dad796d0031
WuTangLan1 <finnmassari16@gmail.com>
2024-05-19 14:59:04 +0200

    save good start

--- src/components/homeDir/bottomFooter.vue
+++ src/components/homeDir/bottomFooter.vue
@@ -1,4 +1,4 @@
-<!-- src\components\homeDir\bottomFooter.vue -->
+<!-- src/components/homeDir/bottomFooter.vue -->
 <script>
 import { ref } from 'vue';
 
@@ -24,101 +24,66 @@ export default {
     };
   }
 }
-
 </script>
 
 <template>
-    <footer class="bottom-footer">
-      <div class="footer-content">
-        <div class="footer-links">
-          <router-link @click.native="$emit('openModal', 'AboutSection')" to="#">About Us</router-link>
-          <router-link @click.native="$emit('openModal', 'TosSection')" to="#">Terms of Service</router-link>
-          <router-link @click.native="$emit('openModal', 'PrivacySection')" to="#">Privacy Policy</router-link>
-        </div>
-        <div class="social-media-icons">
-          <a href="https://twitter.com/CoPlaylist" target="_blank">
-            <img src="@/assets/images/footer/x_icon.png" alt="Twitter">
-          </a>
-          <a href="https://facebook.com/CoPlaylist" target="_blank">
-            <img src="@/assets/images/footer/facebook_icon.png" alt="Facebook">
-          </a>
-          <a href="https://instagram.com/CoPlaylist" target="_blank">
-            <img src="@/assets/images/footer/instagram_icon.png" alt="Instagram">
-          </a>
-        </div>
-      </div>
-      <div class="copy-right">
-        © 2024 CoPlaylist, Inc
-      </div>
-    </footer>
-  </template>
-  <style scoped>
-  @import '@/assets/css/styles.css';
-  
-  .bottom-footer {
-    display: flex;
-    margin-top: 20px;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 20px;
-    background-color: #C3D7F8;
-    color: black;
-    text-align: center;
-    box-shadow: 0 -4px 6px rgba(0, 0, 0, 0.1);
+  <v-footer class="bottom-footer" padless>
+    <v-container fluid>
+      <v-row class="footer-content" justify="center">
+        <v-col cols="4" md="4" class="footer-links">
+          <v-btn text @click="$emit('openModal', 'AboutSection')" class="footer-link">About</v-btn>
+          <v-btn text @click="$emit('openModal', 'TosSection')" class="footer-link">TOS</v-btn>
+          <v-btn text @click="$emit('openModal', 'PrivacySection')" class="footer-link">Privacy</v-btn>
+        </v-col>
+        <v-col cols="12" md="4" class="copy-right">
+          © 2024 CoPlaylist, Inc
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<style scoped>
+.bottom-footer {
+  background-color: #1976D2;
+  color: black;
+  text-align: center;
+  box-shadow: 0 -4px 6px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+}
+
+.footer-content {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.footer-links {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  font-size: 0.9rem;
+}
+
+.footer-link {
+  color: black;
+  text-decoration: none;
+}
+
+.footer-link:hover {
+  text-decoration: underline;
+}
+
+.copy-right {
+  margin-top: 10px;
+  font-size: 0.8rem;
+  color: white;
+}
+
+@media (min-width: 768px) {
+  .footer-links, .social-media-icons, .copy-right {
+    margin-top: 0;
   }
-  
-  .footer-content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 15px;
-  }
-  
-  .footer-links {
-    display: flex;
-    gap: 20px;
-    font-size: 0.9rem;
-  }
-  
-  .social-media-icons {
-    display: flex;
-    gap: 3rem;
-  }
-  
-  .footer-links a,
-  .social-media-icons a {
-    color: black;
-    text-decoration: none;
-  }
-  
-  .social-media-icons img {
-    width: 3rem; /* Smaller size for icons */
-    height: auto; /* Maintain aspect ratio */
-  }
-  
-  .footer-links a:hover,
-  .social-media-icons a:hover {
-    text-decoration: underline;
-  }
-  
-  .copy-right {
-    margin-top: 10px;
-    font-size: 0.8rem;
-  }
-  
-  @media (min-width: 768px) {
-    .bottom-footer {
-      flex-direction: row;
-      justify-content: space-between;
-      padding: 20px 50px;
-    }
-    .footer-content {
-      flex-direction: row;
-      justify-content: space-between;
-      width: 100%;
-      max-width: 1200px;
-    }
-  }
-  </style>
-  
+}
+</style>
