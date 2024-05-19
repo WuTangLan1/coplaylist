d164413ac8de7ad21843437cc1a18467bc6d9877
WuTangLan1 <finnmassari16@gmail.com>
2024-05-19 15:15:30 +0200

    close button working on modal well.

--- src/components/homeDir/info-section/infoModal.vue
+++ src/components/homeDir/info-section/infoModal.vue
@@ -49,7 +49,7 @@ export default {
       <v-card-title class="d-flex align-center">
         <span class="headline">Information Center</span>
         <v-spacer></v-spacer>
-        <v-btn icon @click="closeModal">
+        <v-btn icon @click="closeModal" class="close-btn">
           <v-icon>mdi-close</v-icon>
         </v-btn>
       </v-card-title>
@@ -72,5 +72,25 @@ export default {
   font-size: 1.8rem;
   font-weight: bold;
   color: #333;
+}
+
+
+.v-btn.icon {
+  padding: 0;
+}
+
+.close-btn {
+  background-color: #f44336;
+  color: #ffffff;
+  border-radius: 50%;
+  min-width: 36px;
+  min-height: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.close-btn:hover {
+  background-color: #d32f2f;
 }
 </style>
