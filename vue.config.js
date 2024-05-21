5d914b784205b83a511e257de3c590b8c4bbd847
Finn <finnmassari16@gmail.com>
2024-05-21 15:13:44 +0200

    quick save

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@
 
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify' // Ensures Vuetify components are properly transpiled
-  ],
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 })
