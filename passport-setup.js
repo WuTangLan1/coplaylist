671c3545a1094b89646d88238cc3bdc02b41937a
Finn <finnmassari16@gmail.com>
2024-05-25 17:40:09 +0200

    should now work

--- passport-setup.js
+++ passport-setup.js
@@ -1,11 +1,14 @@
 // passport-setup.js
+require('dotenv').config();
 const passport = require('passport');
 const SpotifyStrategy = require('passport-spotify').Strategy;
+
+const callbackURL = process.env.NODE_ENV === 'production' ? 'https://www.coplaylist.com/callback' : 'http://localhost:3000/callback';
 
 passport.use(new SpotifyStrategy({
   clientID: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/callback"
+  callbackURL: callbackURL 
 },
 function(accessToken, refreshToken, expires_in, profile, done) {
   done(null, { profile: profile, accessToken: accessToken, refreshToken: refreshToken, expires_in: expires_in });
@@ -19,6 +22,4 @@ passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
 
-
 module.exports = passport;
-
