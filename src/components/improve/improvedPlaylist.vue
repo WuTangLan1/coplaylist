4636a422205dd4805feb5ddabb21effa7547a6d5
Finn <finnmassari16@gmail.com>
2024-06-07 12:27:47 +0200

    fixed warning

--- src/components/improve/improvedPlaylist.vue
+++ src/components/improve/improvedPlaylist.vue
@@ -1,25 +1,5 @@
 <!-- src\components\improve\improvedPlaylist.vue -->
-<template>
-    <v-card>
-      <v-card-title>Improved Playlist</v-card-title>
-      <v-card-text>
-        <v-btn @click="improvePlaylist">Improve Playlist</v-btn>
-        <v-list dense v-if="improvedTracks.length">
-          <v-list-item
-            v-for="track in improvedTracks"
-            :key="track.id"
-          >
-            <v-list-item-content>
-              <v-list-item-title>{{ track.name }}</v-list-item-title>
-              <v-list-item-subtitle>{{ track.artist }}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-        <div v-else>No improvements made yet</div>
-      </v-card-text>
-    </v-card>
-  </template>
-  
+ 
   <script>
   export default {
     data() {
@@ -35,6 +15,24 @@
   }
   </script>
   
+  <template>
+    <v-card>
+      <v-card-title>Improved Playlist</v-card-title>
+      <v-card-text>
+        <v-btn @click="improvePlaylist">Improve Playlist</v-btn>
+        <v-list dense v-if="improvedTracks.length">
+          <v-list-item
+            v-for="track in improvedTracks"
+            :key="track.id"
+          >
+            <v-list-item-title>{{ track.name }}</v-list-item-title>
+            <v-list-item-subtitle>{{ track.artist }}</v-list-item-subtitle>
+          </v-list-item>
+        </v-list>
+        <div v-else>No improvements made yet</div>
+      </v-card-text>
+    </v-card>
+  </template>
   
   <style scoped>
   </style>
--- src/components/improve/uploadedPlaylist.vue
+++ src/components/improve/uploadedPlaylist.vue
@@ -1,25 +1,5 @@
 <!-- src\components\improve\uploadedPlaylist.vue -->
-<template>
-    <v-card>
-      <v-card-title>Uploaded Playlist</v-card-title>
-      <v-card-text>
-        <v-btn @click="uploadPlaylist">Upload Playlist</v-btn>
-        <v-list dense v-if="playlist">
-          <v-list-item
-            v-for="track in playlist.tracks"
-            :key="track.id"
-          >
-            <v-list-item-content>
-              <v-list-item-title>{{ track.name }}</v-list-item-title>
-              <v-list-item-subtitle>{{ track.artist }}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-        <div v-else>No playlist loaded</div>
-      </v-card-text>
-    </v-card>
-  </template>
-  
+ 
   <script>
   export default {
     data() {
@@ -34,6 +14,25 @@
     }
   }
   </script>
+
+<template>
+    <v-card>
+      <v-card-title>Uploaded Playlist</v-card-title>
+      <v-card-text>
+        <v-btn @click="uploadPlaylist">Upload Playlist</v-btn>
+        <v-list dense v-if="playlist">
+          <v-list-item
+            v-for="track in playlist.tracks"
+            :key="track.id"
+          >
+            <v-list-item-title>{{ track.name }}</v-list-item-title>
+            <v-list-item-subtitle>{{ track.artist }}</v-list-item-subtitle>
+          </v-list-item>
+        </v-list>
+        <div v-else>No playlist loaded</div>
+      </v-card-text>
+    </v-card>
+  </template>
   
   <style scoped>
   </style>
